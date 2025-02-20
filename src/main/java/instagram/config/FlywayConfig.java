4ef5285807501aaf1da4fced53c234605859bb39
Nurislam Toygonbaev <ntoygonbaev098@gmail.com>
2025-02-20 10:03:56 +0600

    lasttttt

--- src/main/java/instagram/config/FlywayConfig.java
+++ src/main/java/instagram/config/FlywayConfig.java
@@ -1,45 +1,45 @@
-package instagram.config;
-
-import org.flywaydb.core.Flyway;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
-
-@Configuration
-public class FlywayConfig {
-    @Value("${spring.datasource.url}")
-    private String url;
-    @Value("${spring.datasource.username}")
-    private String userName;
-    @Value("${spring.datasource.password}")
-    private String password;
-    @Value("${spring.flyway.baseline-on-migrate}")
-    private boolean baselineOnMigrate;
-    @Value("${spring.flyway.enabled}")
-    private boolean flywayEnabled;
-
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUrl(url);
-        dataSource.setUsername(userName);
-        dataSource.setPassword(password);
-        return dataSource;
-    }
-
-    @Bean
-    public Flyway flyway() {
-        Flyway flyway = Flyway.configure()
-                .dataSource(dataSource())
-                .locations("classpath:migration")
-                .baselineOnMigrate(baselineOnMigrate)
-                .load();
-        if (flywayEnabled) {
-            flyway.migrate();
-        }
-        return flyway;
-    }
-}
+//package instagram.config;
+//
+//import org.flywaydb.core.Flyway;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.jdbc.datasource.DriverManagerDataSource;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//public class FlywayConfig {
+//    @Value("${spring.datasource.url}")
+//    private String url;
+//    @Value("${spring.datasource.username}")
+//    private String userName;
+//    @Value("${spring.datasource.password}")
+//    private String password;
+//    @Value("${spring.flyway.baseline-on-migrate}")
+//    private boolean baselineOnMigrate;
+//    @Value("${spring.flyway.enabled}")
+//    private boolean flywayEnabled;
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setUrl(url);
+//        dataSource.setUsername(userName);
+//        dataSource.setPassword(password);
+//        return dataSource;
+//    }
+//
+//    @Bean
+//    public Flyway flyway() {
+//        Flyway flyway = Flyway.configure()
+//                .dataSource(dataSource())
+//                .locations("classpath:migration")
+//                .baselineOnMigrate(baselineOnMigrate)
+//                .load();
+//        if (flywayEnabled) {
+//            flyway.migrate();
+//        }
+//        return flyway;
+//    }
+//}
